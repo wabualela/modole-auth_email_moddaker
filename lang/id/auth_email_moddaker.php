d2eb4c3be4989636e7cb8d0f152f3a9db5155b41
Wail Abualela <19847088+wabualela@users.noreply.github.com>
2025-02-17 19:29:51 +0300

    Add Indonesian language pack for Email moddaker plugin

---
+++ lang/id/auth_email_moddaker.php
@@ -0,0 +1,50 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * English language pack for Email moddaker
+ *
+ * @package    auth_email_moddaker
+ * @category   string
+ * @copyright  2025 Wail Abualela wailabualela@gmail.com
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'Pendaftaran mandiri berbasis Email Moddaker';
+
+$string['gender']        = 'Jenis Kelamin';
+$string['missinggender'] = 'Jenis kelamin tidak diisi';
+$string['selectgender']  = 'Pilih jenis kelamin';
+$string['male']          = 'Laki-laki';
+$string['female']        = 'Perempuan';
+
+$string['nationality']        = 'Kewarganegaraan';
+$string['missingnationality'] = 'Kewarganegaraan tidak diisi';
+
+$string['dob']        = 'Tanggal Lahir';
+$string['missingdob'] = 'Tanggal lahir tidak diisi';
+
+$string['missingcountry'] = 'Negara tidak diisi';
+
+$string['phonenumber']        = 'Nomor Telepon (contoh: +966501234567)';
+$string['missingphonenumber'] = 'Nomor telepon tidak diisi';
+$string['invalidphonenumber'] = 'Mohon masukkan nomor telepon yang valid dengan kode negara.';
+
+$string['certfullname']        = 'Nama lengkap yang akan muncul di sertifikat';
+$string['missingcertfullname'] = 'Nama lengkap sertifikat tidak diisi';
+$string['invalidcertfullname'] = 'Mohon masukkan nama lengkap yang valid untuk sertifikat.';
